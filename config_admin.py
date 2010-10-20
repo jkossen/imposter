8331826a9b79bb1eeaba5b25094b28980223ae56
Jochem Kossen <jochem.kossen@gmail.com>
2010-10-20 19:14:08 +0200

    refactor to use the flaskjk libs. I had too much duplicate code.
    Biggest change is a generic Viewer class, and generic handling of
    templates. Admin templates thus had to be dealed with in the same
    "themed" manner as frontend.
    
    Also, i seperated the config.py in config_admin.py and
    config_frontend.py so I could use the same variable names
    (DATABASE_HOST, PREFIX, THEME, etc.)
    
    The api part still needs to be done.

---
+++ config_admin.py
@@ -0,0 +1,62 @@
+# Debug True/False
+DEBUG = True
+
+# Title of this weblog instance
+TITLE='Imposter'
+
+# Description of this weblog instance
+DESCRIPTION='Another weblog'
+
+# Link to home page
+FRONTEND_BASEURL='http://CHANGE_THIS.ORG/imposter'
+
+# Database connection strings, example: postgres://imposter:mysecretpassword@localhost:5432/imposter
+DATABASE = 'postgresql://imposter:imposter@localhost/imposter'
+
+# Theme
+THEME = 'default'
+
+# Show summaries instead of full posts in feed content, post lists and front page?
+SUMMARIES = True
+
+# How many items do you want in feeds?
+FEEDITEMS = 10
+
+# Summary size in characters
+SUMMARY_SIZE = 200
+
+# Table prefix
+TABLEPREFIX = 'imposter_'
+
+# Host on which admin runs
+HOST = '192.168.1.3'
+
+# Port on which app runs
+PORT = 5002
+
+# If you use FastCGI, this specifies the path to the socket for the admin
+FCGI_SOCKET = '/var/lib/imposter/admin.sock'
+
+# Secret string used for secure hashing, you could use os.urandom(20)
+SECRET_KEY = 'CHANGE_THIS'
+
+# Frontend prefix, ie the 'posts/' in http://yourweblog.net/posts/
+PREFIX=''
+
+# Date format to use in URL's. Don't set it to an empty string, adjust FRONTEND_ROUTES instead.
+URL_DATE_FORMAT = '%Y/%m/%d'
+
+# Date / time format to use in templates.
+POST_DATETIME_FORMAT = '%Y-%m-%d %H:%M'
+
+# Routes to the view functions in the admin
+ROUTES = {
+    'static_files': 'static/<path:filename>',
+    'index': '',
+    'login': 'login/',
+    'logout': 'logout/',
+    'new_post': 'edit/',
+    'edit_post': 'edit/<int:post_id>/',
+    'save_new_post': 'save/',
+    'save_post': 'save/<int:post_id>/',
+    }
---
+++ config_frontend.py
@@ -0,0 +1,60 @@
+# Debug True/False
+DEBUG = True
+
+# Title of this weblog instance
+TITLE='Imposter'
+
+# Description of this weblog instance
+DESCRIPTION='Another weblog'
+
+# Link to home page
+FRONTEND_BASEURL='http://CHANGE_THIS.ORG/imposter'
+
+# Database connection strings, example: postgres://imposter:mysecretpassword@localhost:5432/imposter
+DATABASE = 'postgresql://imposter:imposter@localhost/imposter'
+
+# Theme
+THEME = 'default'
+
+# Show summaries instead of full posts in feed content, post lists and front page?
+SUMMARIES = True
+
+# How many items do you want in feeds?
+FEEDITEMS = 10
+
+# Summary size in characters
+SUMMARY_SIZE = 200
+
+# Table prefix
+TABLEPREFIX = 'imposter_'
+
+# Host on which frontend runs
+HOST = '192.168.1.3'
+
+# Port on which frontend runs
+PORT = 5000
+
+# If you use FastCGI, this specifies the path to the socket for the frontend
+FCGI_SOCKET = '/var/lib/imposter/frontend.sock'
+
+# Secret string used for secure hashing, you could use os.urandom(20)
+SECRET_KEY = 'CHANGE_THIS'
+
+# Frontend prefix, ie the 'posts/' in http://yourweblog.net/posts/
+PREFIX='imposter/'
+
+# Date format to use in URL's. Don't set it to an empty string, adjust FRONTEND_ROUTES instead.
+URL_DATE_FORMAT = '%Y/%m/%d'
+
+# Date / time format to use in templates.
+POST_DATETIME_FORMAT = '%Y-%m-%d %H:%M'
+
+# Routes to the view functions in the frontend
+ROUTES = {
+    'static_files': 'static/<path:filename>',
+    'index': '',
+    'show_post': '<path:post_date>/<slug>.html',
+    'show_rss': 'feed/rss/',
+    'show_atom': 'feed/atom/',
+    'postlist_by_tag': 'tag/<tag>/',
+    }
